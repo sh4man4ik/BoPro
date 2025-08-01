0ad0dbe8ae3048bb24af2ab545eaab282012b6d5
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-08-01 17:18:51 +0300

    Eslint update 3

--- eslint.config.js
+++ eslint.config.js
@@ -6,10 +6,29 @@ export default [
 		languageOptions: {
 			ecmaVersion: 'latest',
 			sourceType: 'module',
-			env: {
-				browser: true,
-				node: true,
-				es2021: true
+			globals: {
+				window: 'readonly',
+				document: 'readonly',
+				localStorage: 'readonly',
+				navigator: 'readonly',
+				location: 'readonly',
+				setTimeout: 'readonly',
+				clearTimeout: 'readonly',
+				setInterval: 'readonly',
+				clearInterval: 'readonly',
+				requestAnimationFrame: 'readonly',
+				cancelAnimationFrame: 'readonly',
+				Audio: 'readonly',
+
+				require: 'readonly',
+				module: 'readonly',
+				process: 'readonly',
+				__dirname: 'readonly',
+
+				console: 'readonly',
+				URL: 'readonly',
+				URLSearchParams: 'readonly',
+				globalThis: 'readonly'
 			},
 			parserOptions: {
 				ecmaFeatures: {
